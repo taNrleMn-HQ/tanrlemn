ee2fdc69ff04ee65fd2a9e043ac123dbcf97bf85
tanneratgraston <48069584+tanrlemn@users.noreply.github.com>
2024-02-06 16:32:22 -0600

    update callback route.ts

--- app/_lib/hooks/useOrigin.js
+++ app/_lib/hooks/useOrigin.js
@@ -16,7 +16,7 @@ export function useOrigin() {
   const callbackUrl = MODE === 'production' ? removePort : origin;
   const fullPagePath = `${callbackUrl}${pathname}`;
 
-  console.log('window.location', window.location);
+  console.log('callbackUrl', callbackUrl);
 
   return { pathname, callbackUrl, fullPagePath };
 }
